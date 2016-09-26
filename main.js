e8226c8ba3cbf5132d0f37eabe99029fd41a5f9b
Damian Heaton <d.s.heaton@outlook.com>
2016-09-26 22:57:37 +0100

    Experimenting with pushing new messages to render-window.

--- main.js
+++ main.js
@@ -3,15 +3,29 @@ const irc = require("irc");
 
 // prevent JS garbage collector killing the window.
 let win;
+let contents;
 
 function newWindow(){
     win = new BrowserWindow({width: 800, height: 600});
+    contents = win.webContents;
 
     win.loadURL("file://" + __dirname + "/app/index.html");
+
+    contents.on("did-finish-load", function(){
+        contents.send("pingchan", "woop!!");
+        contents.send("newmsg", "<p>Hey, Damo!</p><p class=\"meta\">Meepers42, 22:38 26/09/2016 <span class=\"msgId\">#3</span></p>");
+        sendMsg("This is a test message.", "sysTest", "22:55 26/09/2016");
+    });
 
     win.on("closed", function(){
         win = null;
     });
+}
+
+function sendMsg(content, sender, time){
+    if(contents !== null){
+        contents.send("newmsg", "<p>" + content + "</p><p class=\"meta\">" + sender + ", " + time + "</p>");
+    }
 }
 
 app.on("ready", newWindow);
