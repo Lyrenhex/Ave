dffcd4fcf9fb0dcf69b4a784b9efeab8bfb2d614
Damian Heaton <d.s.heaton@outlook.com>
2016-09-27 22:03:47 +0100

    Accept IRC chat messages and post them to the render window.

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const irc = require("irc");
 // prevent JS garbage collector killing the window.
 let win;
 let contents;
+let client;
 
 function newWindow(){
     win = new BrowserWindow({width: 800, height: 600});
@@ -14,18 +15,36 @@ function newWindow(){
     contents.on("did-finish-load", function(){
         contents.send("pingchan", "woop!!");
         contents.send("newmsg", "<p>Hey, Damo!</p><p class=\"meta\">Meepers42, 22:38 26/09/2016 <span class=\"msgId\">#3</span></p>");
-        sendMsg("This is a test message.", "sysTest", "22:55 26/09/2016");
+        sendMsg("This is a test message.", "Sys", "SystemTest");
+        sendMsg("Connecting to IRC server...", "Sys", "System");
+
+        client = new irc.Client("orwell.freenode.net", "AveTest");
+        sendMsg("Connected!", "Sys", "System");
+
+        client.addListener("message", function (from, to, message){
+            sendMsg(message, to, from);
+        });
+
+        client.addListener('error', function(message) {
+            sendMsg('error: ' + message.toString(), "Sys", "ErrHandler");
+        });
     });
 
     win.on("closed", function(){
+        client.send("QUIT", "testing");
         win = null;
     });
 }
 
-function sendMsg(content, sender, time){
+function sendMsg(content, recip, sender, bgcolour="white"){
     if(contents !== null){
-        contents.send("newmsg", "<p>" + content + "</p><p class=\"meta\">" + sender + ", " + time + "</p>");
+        var d = new Date();
+        contents.send("newmsg", "<p>" + content + "</p><p class=\"meta\">" + sender + "; " + d.toUTCString() + "</p>");
     }
+}
+
+function addUser(username, perms){
+    contents.send("addusr", username + "|" + perms);
 }
 
 app.on("ready", newWindow);
