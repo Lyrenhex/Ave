b8bc6ee8192820b2d1b308e0c00de4f73dfad1aa
Damian Heaton <d.s.heaton@outlook.com>
2016-10-16 23:05:53 +0100

    Add "server profiles".
    Allow users to have multiple server's information stored on their
    computer, and load up a particular one that they want.

--- app/connect.js
+++ app/connect.js
@@ -1,18 +1,62 @@
 const electron = require("electron");
 const fs = require("fs");
 
-var advanced = false;
+// ensure server folder already exists
+fs.mkdir("servers", 0777, function(err){
+        if(err){
+            if(err.code == "EEXIST"){
+                // do nothing; folder already exists
+            }else{
+                console.log("Unable to create servers folder", err);
+            }
+        }
+});
+
+var Servers = [];
+var servers = fs.readdirSync("servers/");
+for(server in servers){
+    Servers.push(JSON.parse(fs.readFileSync("servers/" + servers[server], "utf-8")));
+}
 
 // we have to import jQuery weirdly because of Electron
 window.$ = window.jQuery = require(__dirname + '/res/js/jquery.min.js');
 
+// handle form togglers
+function toggle(id){
+    var element = document.getElementById(id);
+    if(element.classList.contains("shown")){
+        // if shown, hide it
+        element.classList.remove("shown");
+    }else{
+        // otherwise, show it
+        element.classList.add("shown");
+    }
+}
+
 $(document).ready(function(){
-    try{
+    /* try{
         var settings = JSON.parse(fs.readFileSync('settings.json', 'utf8'));
         popFields(settings);
     }catch(err){
         console.log("unable to open settings.json; probably first run:", err);
-    }
+    } */
+
+    var serverList = document.getElementById("serverList");
+    Servers.forEach(function(server, index){
+        console.log(Servers);
+        console.log(server);
+        var serverEntry = document.createElement("button");
+        serverEntry.className = "mdl-button mdl-js-button mdl-js-ripple-effect";
+        var serverText = document.createTextNode(server.user.nickname + " @ " + server.server.address + " (" + server.server.port + ")");
+        serverEntry.appendChild(serverText);
+        serverEntry.id = index;
+        serverEntry.onclick = function(){
+            popFields(Servers[this.id]);
+        }
+
+        componentHandler.upgradeElement(serverEntry);
+        serverList.appendChild(serverEntry);
+    });
 
     $('#connect').submit(function(){
         // set up the settings array
@@ -42,11 +86,11 @@ $(document).ready(function(){
         // convert it to a JSON array
         var jsonSettings = JSON.stringify(settings, null, 4);
         // write it to a file, to persist for next time
-        fs.writeFile('settings.json', jsonSettings, 'utf8', function(err) {
+        fs.writeFile("servers/" + settings.server.address + '.json', jsonSettings, 'utf8', function(err) {
             if(err) {
                 console.log("couldn't write settings to json file: ", err);
             } else {
-                console.log("settings saved as json: settings.json");
+                console.log("settings saved as json: " + settings.server.address + ".json");
             }
         });
         return false;
@@ -67,13 +111,4 @@ function popFields(json){
     $("#clearColours").prop("checked", json.messages.stripForm);
     $("#logMessages").prop("checked", json.messages.log);
     $("#floodProtect").prop("checked", json.floodProtect);
-}
-
-function toggleAdvanced(){
-    if(advanced){
-        document.getElementById("advanced").classList.remove("shown");
-    }else{
-        document.getElementById("advanced").classList.add("shown");
-    }
-    advanced = !advanced;
 }
