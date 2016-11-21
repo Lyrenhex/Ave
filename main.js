d40e97630dfa22ff488dd98906577c125c038ef9
Damian Heaton <d.s.heaton@outlook.com>
2016-11-21 18:30:28 +0000

    Revert "Squirrel"
    This reverts commit 010e9c1463b6d8588b6987336fc2054b5dbe7f16.

--- main.js
+++ main.js
@@ -16,13 +16,12 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-// if(require("electron-squirrel-startup")) return;
+if(require('electron-squirrel-startup')) return;
 
 const {app, BrowserWindow, Menu, ipcMain, shell} = require("electron");
 const irc = require("irc");
 const fs = require("fs");
 
-<<<<<<< Updated upstream
 // prevent JS garbage collector killing the window.
 let win;
 let opWin;
@@ -37,13 +36,6 @@ String.prototype.replaceAll = function(search, replacement) {
     var target = this;
     return target.replace(new RegExp(search, 'g'), replacement);
 };
-=======
-import handleStartupEvent from './squirrel';
-import { updateShortcuts } from './main/utils/_shortcutManager';
-
-app.setAppUserModelId('com.scratso.ave');
-updateShortcuts();
->>>>>>> Stashed changes
 
 Array.prototype.remove = function() {
     var what, a = arguments, L = a.length, ax;
